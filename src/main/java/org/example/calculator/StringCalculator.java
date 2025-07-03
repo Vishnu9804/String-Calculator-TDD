a3d761dd4272375680b18fa2723279a7ee5c0bc7
Vishnu9804 <burkhawalavishnu@gmail.com>
2025-07-04 02:18:49 +0530

    Step 1 : Added Failing test for Empty String

--- src/main/java/org/example/calculator/StringCalculator.java
+++ src/main/java/org/example/calculator/StringCalculator.java
@@ -1,5 +1,9 @@
 package org.example.calculator;
 
 public class StringCalculator {
-    
+    // Step 1: Bare minimum method for 1st step of TDD
+    public int add(String numbers) {
+        // no logic yet — this will fail the test
+        return -1;
+    }
 }
--- src/test/java/org/example/calculator/StringCalculatorTest.java
+++ src/test/java/org/example/calculator/StringCalculatorTest.java
@@ -1,5 +1,15 @@
 package org.example.calculator;
 
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
 public class StringCalculatorTest {
 
+    // Step 1: Add failing test case for empty string input first
+    @Test
+    public void returnZeroWhenInputIsEmpty() {
+        StringCalculator calculator = new StringCalculator();
+        int result = calculator.add("");
+        assertEquals(0, result);
+    }
 }
