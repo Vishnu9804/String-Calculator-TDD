0c060ae0879276e21a823c7024eb869c4a8dac96
Vishnu9804 <burkhawalavishnu@gmail.com>
2025-07-04 17:23:29 +0530

    Step 1.2: Failing test case for the input having two numbers

--- src/test/java/org/example/calculator/StringCalculatorTest.java
+++ src/test/java/org/example/calculator/StringCalculatorTest.java
@@ -13,11 +13,19 @@ public class StringCalculatorTest {
         assertEquals(0, result);
     }
 
-    // Step 1.1 Add failing test case for single number
+    // Step 1.1: Test case for single number passed
     @Test
     public void returnNumberWhenInputIsSingleNumber() {
         StringCalculator calculator = new StringCalculator();
         int result = calculator.add("1");
         assertEquals(1, result);
+    }
+
+    // Step 1.2: Failing test case for the input having two numbers
+    @Test
+    public void returnSumWhenInputIsTwoNumbers() {
+        StringCalculator calculator = new StringCalculator();
+        int result = calculator.add("1,2");
+        assertEquals(3, result);
     }
 }
