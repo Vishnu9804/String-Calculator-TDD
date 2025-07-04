f06c0c1ebf65c686f90d4a6fd2cc80d25dbd8101
Vishnu9804 <burkhawalavishnu@gmail.com>
2025-07-04 11:48:07 +0530

    Step 1.1 : Added failing test case for single number input

--- src/test/java/org/example/calculator/StringCalculatorTest.java
+++ src/test/java/org/example/calculator/StringCalculatorTest.java
@@ -5,11 +5,19 @@ import static org.junit.Assert.assertEquals;
 
 public class StringCalculatorTest {
 
-    // Step 1: Add failing test case for empty string input first
+    // Step 1: Test case for empty string passed
     @Test
     public void returnZeroWhenInputIsEmpty() {
         StringCalculator calculator = new StringCalculator();
         int result = calculator.add("");
         assertEquals(0, result);
+    }
+
+    // Step 1.1 Add failing test case for single number
+    @Test
+    public void returnNumberWhenInputIsSingleNumber() {
+        StringCalculator calculator = new StringCalculator();
+        int result = calculator.add("1");
+        assertEquals(1, result);
     }
 }
